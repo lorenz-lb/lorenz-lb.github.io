8f0dce42cbd5fb6e9f932f6c13bc59ee1f9e3aa9
Lorenz <35742529+lorenz-lb@users.noreply.github.com>
2025-09-11 01:27:51 +0200

    ssr fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,28 @@
     import { MediaQuery } from "svelte/reactivity";
     import { page } from "$app/state";
 
-    const keys: string[] = [...page.url.searchParams.keys()];
-    console.log(keys);
-
     // #################### Desktop Icons ####################
     import Desktopenvironment from "$lib/components/ui95/desktopenvironment.svelte";
-
+    let keys: string[] = $state([]);
+    let dorender: boolean = $state(false);
     const large = new MediaQuery("min-width: 800px");
+    $effect(() => {
+        keys = [...page.url.searchParams.keys()];
+        dorender = true;
+    });
 </script>
 
-{#if large.current}
-    <div class="w-screen h-screen overflow-hidden fixed">
-        <Desktopenvironment toOpen={keys}></Desktopenvironment>
-    </div>
-{:else}
-    <p>
-        Sorry, this page is only available on desktop because you need a mouse!
-    </p>
+{#if dorender}
+    {#if large.current}
+        <div class="w-screen h-screen overflow-hidden fixed">
+            <Desktopenvironment toOpen={keys}></Desktopenvironment>
+        </div>
+    {:else}
+        <p>
+            Sorry, this page is only available on desktop because you need a
+            mouse!
+        </p>
+    {/if}
 {/if}
 
 <style>
